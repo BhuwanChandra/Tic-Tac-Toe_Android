e66e2ebdf221b48951d259ca6799035f287024e4
BhuwanChandra <bhuwan7779@gmail.com>
2019-08-26 00:56:02 +0530

    updated mainactivity

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
+
+import javax.swing.text.html.ImageView;
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+private ImageView img00,img01,img02;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
